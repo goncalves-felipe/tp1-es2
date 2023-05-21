2e309c9a0be0cd431c48a93b9ce9572b3ff825c5
Pedro Moutinho <phpedromoutinho@gmail.com>
2023-05-21 19:10:36 -0300

    Fix npm start

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Fornecedor } from "./model/Fornecedor/Fornecedor";
-function main(): void {
-    let fornecedores = new Array<Fornecedor>();
-    fornecedores.push(new Fornecedor(1, "João da Silva", "joaodasilva@email.teste", "+99(99)99999-9999", "11.111.111/0001-11"));
-    console.log(fornecedores)
+
+const main = function(): void {
+    let fornecedor = new Fornecedor(1, "João da Silva", "joaodasilva@email.teste", "+99(99)99999-9999", "11.111.111/0001-11");
+
+    console.log(fornecedor);
 }
 
 main();
