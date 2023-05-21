18ea171430371e9e379bd41132a2e465102069a0
Pedro Moutinho <phpedromoutinho@gmail.com>
2023-05-21 17:44:29 -0300

    Readme

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-function main(): void {}
+import { Fornecedor } from "./model/Fornecedor/Fornecedor";
+function main(): void {
+    let fornecedores = new Array<Fornecedor>();
+    fornecedores.push(new Fornecedor(1, "João da Silva", "joaodasilva@email.teste", "+99(99)99999-9999", "11.111.111/0001-11"));
+    console.log(fornecedores)
+}
 
 main();
